22daf7a99c71c6c55b1c63bfbff5d31d478214e7
franc <franc@FRANCIVALDO>
2024-02-19 23:09:00 -0300

    SEGUNDA

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,27 @@
 import java.util.Locale;
-
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-
-		int y = 32;
-		double x = 10.35784;
-		
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		
-				
-		
-		System.out.println(x);
-		System.out.printf("%.2f\n", x);
-		System.out.printf("%.4f%n", x);
 		Locale.setDefault(Locale.US);
-		System.out.printf("%.4f\n", x);
-		System.out.println("RESULTADO = " + x + " METROS");
-		System.out.printf("RESULTADO = %.2f metros\n", x);
 		
-		System.out.printf("%s tem %d anos e ganha RS %.2f reais%n" , nome, idade, renda);
+		Scanner sc = new Scanner(System.in);
 		
-		//JOptionPane.showMessageDialog(null, "RESULTADO = " + x + " METROS");
-
+		String x;
+		int y;
+		double z;
 		
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+				
+		System.out.println("Dados digitados: ");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		
-		
+		sc.close();
 	}
 
 }
